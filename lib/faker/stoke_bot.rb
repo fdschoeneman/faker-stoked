5c7f9e7715db703adbd0d5674b080d400ecd24e5
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2013-12-04 21:04:20 -0800

    try this

--- lib/faker/stoke_bot.rb
+++
@@ -1,6 +0,0 @@
-require 'intro_file.txt'
-require 'object_file.txt'
-require 'subject_file.txt'
-require 'verb_file.txt'
-require 'stoke_bot'
-
--- lib/faker/stoked.rb
+++ lib/faker/stoked.rb
@@ -1,4 +1,8 @@
 require 'debugger'
 require 'faker_bro_bio'
 require 'faker_bro_words'
-
+require 'intro_file.txt'
+require 'object_file.txt'
+require 'subject_file.txt'
+require 'verb_file.txt'
+require 'stoke_bot'
