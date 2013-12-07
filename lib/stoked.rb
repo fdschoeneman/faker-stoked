4c8d9750234da556229858f07bb05122ed06e622
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2013-12-07 15:13:28 -0800

    removed duplicate

--- lib/stoked.rb
+++
@@ -1,9 +0,0 @@
-require 'debugger'
-require 'faker/bio'
-require 'faker/bro_words'
-require 'faker/stokebot'
-require 'faker/stokebot/intro'
-require 'faker/stokebot/verb'
-require 'faker/stokebot/subject'
-require 'faker/stokebot/object'
-
