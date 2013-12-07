7224131b5b8e3c3f1277a8ae75ab1d6a55d7efd6
Frederick D. Schoeneman <fred.schoeneman@gmail.com>
2013-12-07 15:20:39 -0800

    required faker/stoked in test helper

--- test/helper.rb
+++ test/helper.rb
@@ -11,7 +11,7 @@ require 'minitest/autorun'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'stoked'
+require 'faker/stoked'
 
 # class MiniTest::Unit::TestCase
 # end
